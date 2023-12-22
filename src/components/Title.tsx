af12822279938a5105e882defacb4a30e4a6265f
gkatanacio <gkatanacio.swe@gmail.com>
2023-12-22 16:04:55 +0800

    Add margin to title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ const ICON_SIZE = 47;
 
 export const Title = () => {
   return (
-    <h1 className="d-flex justify-content-center border-bottom">
+    <h1 className="d-flex justify-content-center border-bottom mt-1">
       <img src={icon} alt="icon" height={ICON_SIZE} width={ICON_SIZE} />
       &nbsp;Paraphraser
     </h1>
