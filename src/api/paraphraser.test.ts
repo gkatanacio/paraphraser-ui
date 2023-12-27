70f537f94b4608f0a0b8e87a098a25a16cc704b9
gkatanacio <gkatanacio.swe@gmail.com>
2023-12-27 16:16:14 +0800

    Update unit tests for api

--- src/api/paraphraser.test.ts
+++ src/api/paraphraser.test.ts
@@ -7,14 +7,15 @@ import { paraphrase } from "./paraphraser";
 describe("paraphrase", () => {
   it("makes the expected fetch call to server", async () => {
     mock.method(global, "fetch", async (url: string, reqOptions: anyObject) => {
+      expect(reqOptions.method).toEqual("POST");
       expect(url).toEqual(`${import.meta.env.VITE_PARAPHRASER_API_BASE_URL}/paraphrase`);
       expect(reqOptions.headers["Content-Type"]).toEqual("application/json");
 
       return {
         status: 200,
         ok: true,
-        json: async (): Promise<string> => {
-          return "";
+        json: async () => {
+          return {};
         },
       };
     });
@@ -24,6 +25,33 @@ describe("paraphrase", () => {
       tone: "formal",
       text: "I'm hungry. What's for dinner?",
     });
+
+    mock.reset();
+  });
+
+  it("throws error from response", async () => {
+    const dummyError = "some error";
+    
+    mock.method(global, "fetch", async () => {
+      return {
+        status: 400,
+        ok: false,
+        json: async () => {
+          return { error: dummyError };
+        },
+        headers: {
+          get: () => {
+            return "application/json";
+          },
+        },
+      };
+    });
+
+    await expect(() => paraphrase({
+      provider: "chatgpt",
+      tone: "formal",
+      text: "Hey there",
+    })).rejects.toThrowError(dummyError);
 
     mock.reset();
   });
